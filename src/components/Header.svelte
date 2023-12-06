41a9deaa4282fe222701817c16d6f209d3249715
salvatorestampone <salvatore.stampone@brots.cloud>
2023-12-06 23:05:31 +0100

    feat: init layout, fonts, toTop arrow

---
+++ src/components/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+	import classNames from 'classnames';
+	export let y;
+
+	let tabs = [
+		{ name: 'Projects', link: '#projects' },
+		{ name: 'About Me', link: '#about' }
+	];
+</script>
+
+<header
+	class={classNames(
+		'sticky top-0 z-10 flex items-center justify-between border border-solid px-6 duration-200',
+		y > 0 ? 'border-blue-950 bg-slate-950 py-4' : 'border-transparent bg-transparent py-6'
+	)}
+>
+	<h1>
+		<strong>Salvatore</strong> Stampone
+	</h1>
+	<div class="hidden items-center gap-4 sm:flex">
+		{#each tabs as tab, i}
+			<a href={tab.link} class="font-freshwost transition-colors hover:text-blue-400">{tab.name}</a>
+		{/each}
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import '../app.css';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
+
+	let y;
+	let innerHeight = 0;
+	let innerWidth = 0;
+
+	function goTop() {
+		document.body.scrollIntoView();
+	}
+</script>
+
+<div>
+	<div>
+		<button>
+			<i class="fa-solid fa-arrow-up"></i>
+		</button>
+	</div>
+	<Header {y} />
+	<slot />
+	<Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
