e22913a83b6f30caab6a220243e3732d23543649
salvatorestampone <salvatore.stampone@brots.cloud>
2023-12-10 17:33:27 +0100

    feat(home): add projects section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,26 @@
 <script>
+	import Step from './Step.svelte';
+
+	const steps = [
+		{
+			name: 'SpeedService Lucera',
+			icon: 'fa-solid fa-truck-fast',
+			desc: '<p>Experience seamless shipping solutions with SpeedService Lucera. This <strong class="text-blue-400">Next.js and Tailwind CSS</strong> powered website allows you to effortlessly generate shipping quotes and schedule convenient home pickups.</p>',
+			href: 'https://www.speedservicelucera.com/'
+		},
+		{
+			name: 'Life in Dots',
+			icon: 'fa-solid fa-shield-heart',
+			desc: '<p>Explore Life in Dots, a website built with <strong class="text-blue-400">React, Typescript, and Vite.</strong> Visualize the duration of your life through an engaging dot representation.</p>',
+			href: 'https://life-in-dots-jade.vercel.app/'
+		},
+		{
+			name: 'Ready',
+			icon: 'fa-solid fa-book-open',
+			desc: 'Dive into the literary world with Ready. Utilizing <strong class="text-blue-400">JavaScript and Sass</strong>, this website connects to the OpenLibrary API, enabling you to search for literary genres or embark on a serendipitous journey with the \'Feeling Lucky\' button.',
+			href: 'https://salvatore-stampone.github.io/ready/'
+		}
+	];
 </script>
 
 <main class="flex flex-1 flex-col p-4">
@@ -24,6 +46,28 @@
 		</div>
 		<div class="relative grid place-items-center shadow-2xl">
 			<img src="images/profile.png" alt="Profile" class="z-[2] max-h-[70vh] object-cover" />
+		</div>
+	</section>
+
+	<section id="projects" class="flex flex-col gap-24 py-20 lg:py-32">
+		<div class="flex flex-col gap-2 text-center">
+			<h6 class="text-lg sm:text-xl md:text-2xl">A few of my creative endeavors.</h6>
+			<h3 class="text-3xl sm:text-4xl md:text-5xl">
+				Curious to <span class="text-blue-400">see</span> my work?
+			</h3>
+		</div>
+		<a
+			href="#"
+			target="_blank"
+			class="mx-auto -mb-4 -mt-10 flex items-center gap-2 rounded-md border border-solid border-white px-4 py-2 duration-200 hover:border-blue-700 sm:mb-0"
+		>
+			<i class="fa-regular fa-circle-play"></i>
+			<p>Watch the video</p>
+		</a>
+		<div class="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-10">
+			{#each steps as step}
+				<Step {step} />
+			{/each}
 		</div>
 	</section>
 </main>
---
+++ src/components/Step.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let step;
+</script>
+
+<a
+	href={step.href}
+	target="_blank"
+	class="group flex cursor-pointer flex-col gap-4 rounded-lg border border-solid border-blue-700 p-4 text-center duration-200 hover:border-blue-400 sm:p-6 md:p-8"
+	><div
+		class="mx-auto -mt-10 grid place-items-center bg-slate-950 px-4 text-5xl duration-200 sm:-mt-12 md:-mt-14 md:text-6xl lg:-mt-16"
+	>
+		<i class={step.icon}></i>
+	</div>
+	<h3 class="text-xl sm:text-2xl md:text-3xl">{step.name}</h3>
+	<p class="text-start">{@html step.desc}</p>
+	<div class="flex flex-1 items-center justify-between gap-4">
+		<p class="ml-auto">
+			Go to <span class="inline-block duration-200 group-hover:translate-x-2">&rarr;</span>
+		</p>
+	</div>
+</a>
