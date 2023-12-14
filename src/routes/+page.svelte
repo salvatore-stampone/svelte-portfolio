4a73e652230f49d7e99cbe9ec3fc4a96c67251a8
salvatorestampone <salvatore.stampone@brots.cloud>
2023-12-14 18:01:22 +0100

    fix: add basic SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
 	import Main from '../components/Main.svelte';
 </script>
 
+<svelte:head>
+	<title>Salvatore Stampone | Full Stack Developer</title>
+	<meta
+		name="description"
+		content="I'm Salvatore Stampone, a Full Stack Developer based in Italy, with a preference for the front-end. I work mainly with Next.js and SvelteKit and am in love with TailwindCSS and the cascade!"
+	/>
+</svelte:head>
+
 <Main />
