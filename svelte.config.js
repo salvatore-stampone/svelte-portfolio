7fe2ecf7a6b25d5e2732c841834ef0703568803c
Salvatore Stampone <salvatorestampone@icloud.com>
2025-07-17 18:26:11 +0200

    chore(config): change adapter runtime from nodejs18.x to edge

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs18.x'
+			runtime: 'edge'
 		})
 	},
 	preprocess: vitePreprocess()
 };
+
 export default config;
